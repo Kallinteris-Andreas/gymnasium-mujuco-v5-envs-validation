3424f9c6726dcf245b5c4687293c9ab9ad8a60a2
Kallinteris Andreas <kallinteris@protonmail.com>
2023-05-15 09:13:25 +0300

    update

--- plot_double_pend.py
+++ plot_double_pend.py
@@ -5,16 +5,16 @@ RUNS = 10  # Number of statistical runs
 
 steps = np.load('results/InvertedDoublePendulum_v4_TD3/run_0/evaluations.npz')['timesteps']
 returns_TD3_v4 = np.average(np.array([np.load('results/InvertedDoublePendulum_v4_TD3/run_' + str(run) + '/evaluations.npz')['results'] for run in range(RUNS)]), axis=2)
-#returns_TD3_v4_fix = np.average(np.array([np.load('results/InvertedDoublePendulum_v4_fixed_TD3/run_' + str(run) + '/evaluations.npz')['results'] for run in range(RUNS)]), axis=2)
+returns_TD3_v4_fix = np.average(np.array([np.load('results/InvertedDoublePendulum_v4_fixed_TD3/run_' + str(run) + '/evaluations.npz')['results'] for run in range(RUNS)]), axis=2)
 returns_TD3_v5 = np.average(np.array([np.load('results/InvertedDoublePendulum_v5_TD3/run_' + str(run) + '/evaluations.npz')['results'] for run in range(RUNS)]), axis=2)
 
 fig = plt.figure()
 ax = fig.add_subplot(1, 1, 1)
 ax.plot(steps, np.average(returns_TD3_v4, axis=0), label='v4')
-#ax.plot(steps, np.average(returns_TD3_v4_fix, axis=0), label='v4-fixed')
+ax.plot(steps, np.average(returns_TD3_v4_fix, axis=0), label='v4-fixed')
 ax.plot(steps, np.average(returns_TD3_v5, axis=0), label='v5')
 ax.fill_between(steps, np.min(returns_TD3_v4, axis=0), np.max(returns_TD3_v4, axis=0), alpha=0.2)
-#ax.fill_between(steps, np.min(returns_TD3_v4_fix, axis=0), np.max(returns_TD3_v4_fix, axis=0), alpha=0.2)
+ax.fill_between(steps, np.min(returns_TD3_v4_fix, axis=0), np.max(returns_TD3_v4_fix, axis=0), alpha=0.2)
 ax.fill_between(steps, np.min(returns_TD3_v5, axis=0), np.max(returns_TD3_v5, axis=0), alpha=0.2)
 
 ax.set_title('TD3 on MuJoCo/InvertedDoublePendulum, for ' + str(RUNS) + ' Runs')
