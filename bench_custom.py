402e4235e1d4724545d0db638479e44fcee5ddcf
Kallinteris Andreas <kallinteris@protonmail.com>
2023-05-28 09:58:46 +0300

    add humanoid_standup

--- bench_custom.py
+++ bench_custom.py
@@ -1,6 +1,7 @@
 import gymnasium as gym
-from envs import humanoidstandup_v5
 import numpy as np
+from gymnasium.experimental.wrappers import RescaleActionV0
+import gymnasium.experimental.wrappers.YRescaleActionV0
 
 from stable_baselines3 import TD3
 from stable_baselines3 import PPO
@@ -10,7 +11,7 @@ from stable_baselines3.common.vec_env import SubprocVecEnv
 from stable_baselines3.common.vec_env import DummyVecEnv
 from my_eval import EvalCallback
 
-RUNS = 10  # Number of Statistical Runs
+RUNS = 1  # Number of Statistical Runs
 TOTAL_TIME_STEPS = 2_000_000
 #ALGO = TD3
 ALGO = PPO
@@ -18,14 +19,13 @@ EVAL_SEED = 1234
 EVAL_FREQ = 5000
 EVAL_ENVS = 20
 
-for run in range(8, RUNS):
-    #env = gym.make('HumanoidStandup-v4')
-    #eval_env = gym.make('HumanoidStandup-v4')
-    env = gym.wrappers.TimeLimit(humanoidstandup_v5.HumanoidStandupEnv(), max_episode_steps=1000)
-    eval_env = gym.wrappers.TimeLimit(humanoidstandup_v5.HumanoidStandupEnv(), max_episode_steps=1000)
+for run in range(0, RUNS):
+    env = gym.make('Humanoid-v5', xml_file='/home/master-andreas/casie-scene.xml', healthy_z_range=(1.0, 2.1), render_mode=None)
+    env = RescaleActionV0(env, min_action=-1, max_action=1)
+    eval_env = gym.make('Humanoid-v5', xml_file='/home/master-andreas/casie-scene.xml', healthy_z_range=(1.0, 2.1), render_mode=None)
+    eval_env = RescaleActionV0(eval_env, min_action=-1, max_action=1)
 
-    #eval_path = 'results/HumanoidStandup_v4_PPO/run_' + str(run)
-    eval_path = 'results/HumanoidStandup_v5_PPO/run_' + str(run)
+    eval_path = 'results/cassie/run_' + str(run)
 
     eval_callback = EvalCallback(eval_env, seed=EVAL_SEED, best_model_save_path=eval_path, log_path=eval_path, n_eval_episodes=EVAL_ENVS, eval_freq=EVAL_FREQ, deterministic=True, render=False, verbose=True)
 
--- render_model.py
+++ render_model.py
@@ -6,29 +6,35 @@ from envs import ant_v5
 
 import numpy as np
 
-from stable_baselines3 import TD3
+from stable_baselines3 import TD3, PPO
 from stable_baselines3.common.logger import configure
 from stable_baselines3.common.evaluation import evaluate_policy
 
+#eval_env = gym.wrappers.TimeLimit(ant_v5.AntEnv(use_contact_forces=False, render_mode='human'), max_episode_steps=1000)
+#model = TD3.load(path='/home/master-andreas/rl/project/results/ant_v4_fixed_without_ctn_TD3/run_0/best_model.zip', env=eval_env, device='cpu')
+
+eval_env = gym.make('Humanoid-v5', xml_file='/home/master-andreas/mujoco_menagerie/agility_cassie/scene.xml', healthy_z_range=(1.0, 2.1), render_mode='human')
+model = PPO.load(path='/home/master-andreas/rl/project/results/cassie/run_0/best_model.zip', env=eval_env, device='cpu')
 
 #eval_env = gym.make('InvertedDoublePendulum-v4', render_mode='human')
 #eval_env = gym.wrappers.TimeLimit(inverted_double_pendulum_v4_fixed.InvertedDoublePendulumEnv(render_mode='human'), max_episode_steps=1000)
-eval_env = gym.wrappers.TimeLimit(ant_v5.AntEnv(use_contact_forces=False, render_mode='human'), max_episode_steps=1000)
 #eval_env = gym.wrappers.TimeLimit(ant_v5.AntEnv(use_contact_forces=True, render_mode='human'), max_episode_steps=1000)
 
 #model = TD3.load(path='/home/master-andreas/rl/project/results/InvertedDoublePendulum_v4_TD3/run_0/best_model.zip', env=eval_env, device='cpu')
 #model = TD3.load(path='/home/master-andreas/rl/project/results/InvertedDoublePendulum_v4_fixed_TD3/run_0/best_model.zip', env=eval_env, device='cpu')
-model = TD3.load(path='/home/master-andreas/rl/project/results/ant_v4_fixed_without_ctn_TD3/run_0/best_model.zip', env=eval_env, device='cpu')
 #model = TD3.load(path='/home/master-andreas/rl/project/results/ant_v5_TD3/run_8/best_model.zip', env=eval_env, device='cpu')
 
-evaluate_policy
+#avg_return, std_return = evaluate_policy(model, eval_env, n_eval_episodes=1000)
+#print(f"the average return is {avg_return}")
+
+#breakpoint()
 
 vec_env = model.get_env()
 obs = vec_env.reset()
 for step in range(10000):
     action, _state = model.predict(obs, deterministic=True)
     obs, reward, done, info = vec_env.step(action)
+    print(info)
     time.sleep(0.050)
-
 
 
