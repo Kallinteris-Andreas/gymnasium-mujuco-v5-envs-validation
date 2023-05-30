13f921d2dbe0522519f5c09269d532c696f84383
Kallinteris Andreas <kallinteris@protonmail.com>
2023-05-30 12:38:10 +0300

    update reacher

--- bench_reacher.py
+++ bench_reacher.py
@@ -2,25 +2,28 @@ import gymnasium as gym
 import envs.reacher_v5 as reacher_v5 
 import numpy as np
 
-from stable_baselines3 import TD3
+from stable_baselines3 import TD3, PPO
 from stable_baselines3.common.logger import configure
 #from stable_baselines3.common.callbacks import EvalCallback
 from my_eval import EvalCallback
 
 RUNS = 10  # Number of Statistical Runs
 TOTAL_TIME_STEPS = 400_000
-ALGO = TD3
+#ALGO = TD3
+ALGO = PPO
 EVAL_SEED = 1234
 EVAL_FREQ = 1000
 
 for run in range(0, RUNS):
-    env = gym.make('Reacher-v4')
-    eval_env = gym.make('Reacher-v4')
-    #env = gym.wrappers.TimeLimit(reacher_v5.ReacherEnv(), max_episode_steps=1000)
-    #eval_env = gym.wrappers.TimeLimit(reacher_v5.ReacherEnv(), max_episode_steps=1000)
+    #env = gym.make('Reacher-v4')
+    #eval_env = gym.make('Reacher-v4')
+    env = gym.wrappers.TimeLimit(reacher_v5.ReacherEnv(), max_episode_steps=50)
+    eval_env = gym.wrappers.TimeLimit(reacher_v5.ReacherEnv(), max_episode_steps=50)
 
-    eval_path = 'results/reacher_v4_TD3/run_' + str(run)
-    #eval_path = 'results/reacher_v5_TD3/run_' + str(run)
+    #eval_path = 'results/reacher_v4_TD3/run_' + str(run)
+    eval_path = 'results/reacher_v5_TD3/run_' + str(run)
+    #eval_path = 'results/reacher_v4_PPO/run_' + str(run)
+    #eval_path = 'results/reacher_v5_PPO/run_' + str(run)
 
     eval_callback = EvalCallback(eval_env, seed=EVAL_SEED, best_model_save_path=eval_path, log_path=eval_path, n_eval_episodes=10, eval_freq=EVAL_FREQ, deterministic=True, render=False, verbose=True)
 
--- plot_reacher.py
+++ plot_reacher.py
@@ -5,25 +5,31 @@ RUNS = 10  # Number of statistical runs
 
 steps = np.load('results/reacher_v4_TD3/run_0/evaluations.npz')['timesteps']
 returns_TD3_v4 = np.average(np.array([np.load('results/reacher_v4_TD3/run_' + str(run) + '/evaluations.npz')['results'] for run in range(RUNS)]), axis=2)
-returns_TD3_v5 = np.average(np.array([np.load('results/reacher_v5_TD3/run_' + str(run) + '/evaluations.npz')['results'] for run in range(RUNS)]), axis=2)
+returns_TD3_v5 = np.average(np.array([np.load('results/reacher_v5_TD3/run_' + str(run) + '/evaluations.npz')['results'] for run in range(RUNS)]), axis=2)[:, :400]
+#returns_PPO_v4 = np.average(np.array([np.load('results/reacher_v4_PPO/run_' + str(run) + '/evaluations.npz')['results'] for run in range(RUNS)]), axis=2)
+#returns_PPO_v5 = np.average(np.array([np.load('results/reacher_v5_PPO/run_' + str(run) + '/evaluations.npz')['results'] for run in range(RUNS)]), axis=2)
 
-breakpoint()
+#breakpoint()
 
 fig = plt.figure()
 ax = fig.add_subplot(1, 1, 1)
-ax.plot(steps, np.average(returns_TD3_v4, axis=0), label='v4')
-ax.plot(steps, np.average(returns_TD3_v5, axis=0), label='v5')
+ax.plot(steps, np.average(returns_TD3_v4, axis=0), label='v4 TD3')
+ax.plot(steps, np.average(returns_TD3_v5, axis=0), label='v5 TD3')
+#ax.plot(steps, np.average(returns_PPO_v4, axis=0), label='v4 PPO')
+#ax.plot(steps, np.average(returns_PPO_v5, axis=0), label='v5 PPO')
 ax.fill_between(steps, np.min(returns_TD3_v4, axis=0), np.max(returns_TD3_v4, axis=0), alpha=0.2)
 ax.fill_between(steps, np.min(returns_TD3_v5, axis=0), np.max(returns_TD3_v5, axis=0), alpha=0.2)
+#ax.fill_between(steps, np.min(returns_PPO_v4, axis=0), np.max(returns_PPO_v4, axis=0), alpha=0.2)
+#ax.fill_between(steps, np.min(returns_PPO_v5, axis=0), np.max(returns_PPO_v5, axis=0), alpha=0.2)
 
-ax.set_title('TD3 on MuJoCo/Reacher, for ' + str(RUNS) + ' Runs')
+ax.set_title('SB3 on MuJoCo/Reacher, for ' + str(RUNS) + ' Runs')
 ax.legend()
 
 fig.set_figwidth(16)
 fig.set_figheight(9)
 
-plt.savefig("figures/TD3_Reacher" + ".eps", bbox_inches="tight")
-plt.savefig("figures/TD3_Reacher" + ".png", bbox_inches="tight")
+plt.savefig("figures/Reacher" + ".eps", bbox_inches="tight")
+plt.savefig("figures/Reacher" + ".png", bbox_inches="tight")
 
 fig.show()
 breakpoint()
